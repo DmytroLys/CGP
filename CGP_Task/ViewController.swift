b4971b706da0695eea0d9536189872470354a822
Dmytro Lyshtva <lyshtva.dmytro@gmail.com>
2023-09-12 20:19:09 +0200

    initial commit

---
+++ CGP_Task/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CGP_Task
+//
+//  Created by Dmytro Lyshtva on 12.09.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+        
+        override func viewDidLoad() {
+            super.viewDidLoad()
+            
+        }
+
+
+}
+
